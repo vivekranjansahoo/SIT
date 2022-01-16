249196d52451959649a806604d56a8f6c281ad9f
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-01-16 13:38:02 +0530

    Create 3_temp_fah_cel.c

---
+++ 1st Year/1st Sem/Mid/Lab/Assignment 2/Program/3_temp_fah_cel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float celsius,fahrenheit;
+	
+	printf("Enter temperature in fahrenheit :  ");
+	scanf("%f",&fahrenheit);
+	
+	celsius=(fahrenheit-32)*5/9;
+	
+	printf("\nTemperature in celsius is %.2f\n ",celsius);
+	
+	return 0;
+}
