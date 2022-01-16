6606aa337efac20657e4aa3776e7a21bc48838ce
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-01-16 13:37:04 +0530

    Create 2_Sum_Avarage.c

---
+++ 1st Year/1st Sem/Mid/Lab/Assignment 2/Program/2_Sum_Avarage.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum;
+	float avg;
+	printf("Enter the first number : ");
+	scanf("%d",&a);
+	printf("Enter the second number : ");
+	scanf("%d",&b);
+	
+	sum=a+b;
+	avg=(float)(a+b)/2;
+	
+	printf("\nThe sum of %d and %d is = %d\n" ,a,b,sum);
+	printf("\nThe avarage of %d and  %d is = %f",a,b,avg);
+	
+	return 0;
+}
