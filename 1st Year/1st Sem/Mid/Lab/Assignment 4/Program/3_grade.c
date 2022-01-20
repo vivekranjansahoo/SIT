03b13e0efc0de209e6c60f2a2d58e01897239003
vivekranjansahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2022-01-20 16:00:30 +0530

    Create 3_grade.c

---
+++ 1st Year/1st Sem/Mid/Lab/Assignment 4/Program/3_grade.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int marks;
+    printf("Enter your marks ");
+    scanf("%d", &marks);
+    if (marks < 0 || marks > 100)
+    {
+        printf("Wrong Entry");
+    }
+    else if (marks < 40)
+    {
+        printf("Grade F");
+    }
+    else if (marks >= 40 && marks < 50)
+    {
+        printf("Grade D");
+    }
+    else if (marks >= 50 && marks < 60)
+    {
+        printf("Grade C");
+    }
+    else if (marks >= 60 && marks < 70)
+    {
+        printf("Grade B");
+    }
+    else if (marks >= 70 && marks < 80)
+    {
+        printf("Grade A");
+    }
+    else if (marks >= 80 && marks < 90)
+    {
+        printf("Grade E");
+    }
+    else
+    {
+        printf("Grade O");
+    }
+    return 0;
+}
